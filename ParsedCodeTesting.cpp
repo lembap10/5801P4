df2d53306130af629ac0cc8f66663c02f488a7af
Young Xu <xu000576@umn.edu>
2022-11-20 16:09:50 -0600

    added testing files

---
+++ ParsedCodeTesting.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <main.cpp>
+#include <include/ParsedCode.h>
+#include <include/DefaultCodeParser.h>
+#include <include/Parser.h>
+#include <src/DefaultCodeParser.cc>
+#include <src/ParsedCode.cc>
+using namespace std;
+
+int main(){
+    return 0;
+};
