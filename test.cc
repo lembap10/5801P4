fec51f46bf0d77cba0840fda29695d7a9eced6d8
Young Xu <xu000576@umn.edu>
2022-11-20 15:41:41 -0600

    test files for .cc, .h, .py

---
+++ test.cc
@@ -0,0 +1,17 @@
+// {START
+// This is a test file for testing PPALMS Program
+#include <iostream>
+using namespace std;
+
+int main(){
+    // START}
+    // {*
+    int x = 0;
+    int y = 0;
+    // *}
+    int z = x + y;
+    cout << z;
+    // {END
+    return z;
+}
+// END}
---
+++ test.py
@@ -0,0 +1,13 @@
+# {START
+# This is a test file for testing PPALMS Program
+
+def main():
+    # START}
+    # {*
+    x = 0
+    y = 0
+    # *}
+    z = x + y
+    print(z)
+    # {END
+# END}
