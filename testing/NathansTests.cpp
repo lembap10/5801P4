76ced233e185f3998236af653dde4497768a9e71
NathanL0518 <chenxiliang18@gmail.com>
2022-11-21 16:59:11 -0600

    Added nathan test cases for print

--- testing/NathansTests.cpp
+++ testing/NathansTests.cpp
@@ -1,7 +1,53 @@
 #include "tester.h"
+#include <string>   // std::string
+#include <iostream> // std::cout
+#include <sstream>  // std::stringstream
 
 using namespace std;
 
-void NLTests(){
+void PrintTest(ParsedCode code1, ParsedCode code2){
+    // PRINT TESTING for equal outputs
+    stringstream buffer;
+    streambuf* prevcoutbuf = cout.rdbuf(buffer.rdbuf());
+    code1.print();
+    cout.rdbuf(prevcoutbuf);
+    string actual = buffer.str();
+
+    buffer = stringstream();
+    prevcoutbuf = cout.rdbuf(buffer.rdbuf());
+    code2.print();
+    cout.rdbuf(prevcoutbuf);
+    string expect = buffer.str();
+    
+    test_equivalence(&actual, &expect, "string", "Test #1: Testing 2 equal outputs(Expect pass)");
+
+    // PRINT TESTING for unequal outputs
+    code1.AddCodeTuple("1", false);
+    buffer = stringstream();
+    prevcoutbuf = cout.rdbuf(buffer.rdbuf());
+    code1.print();
+    cout.rdbuf(prevcoutbuf);
+    actual = buffer.str();
+
+    buffer = stringstream();
+    prevcoutbuf = cout.rdbuf(buffer.rdbuf());
+    code2.print();
+    cout.rdbuf(prevcoutbuf);
+    expect = buffer.str();
+    
+    test_equivalence(&actual, &expect, "string", "Test #1: Testing 2 not equal outputs(Expect fail)");
 
 }
+
+void NLTests(){
+
+    ParsedCode code1 = ParsedCode();
+    ParsedCode code2 = ParsedCode();
+    code1.AddCodeTuple("line of code 1", false);
+    code2.AddCodeTuple("line of code 1", false);
+    code1.AddCodeTuple("anotha line", false);
+    code1.RemoveCodeTuple(1);
+    cout << "Begin PRINT Test" << endl;
+    PrintTest(code1, code2);
+}
+
