3a43eefb26f3f655b9f3067c4d549b6107ff5738
Young Xu <xu000576@umn.edu>
2022-11-23 13:40:51 -0600

    added descriptions

--- testing/YoungsTests.cpp
+++ testing/YoungsTests.cpp
@@ -17,9 +17,9 @@ void testConstructor(){
     vector<bool> ignoredLines;
     ignoredLines = parser.getIgnores();
     vector<bool> expectediglines;
-    test_equivalence(&numLines, &expectedLines, "int", "PC200");
-    test_equivalence(&reglines, &expectedreglines, "vector<string>", "PC201");
-    test_equivalence(&ignoredLines, &expectediglines, "vector<bool>", "PC202");
+    test_equivalence(&numLines, &expectedLines, "int", "PC200: numLines attribute successfully initialized");
+    test_equivalence(&reglines, &expectedreglines, "vector<string>", "PC201: lines attribute successfully initialized");
+    test_equivalence(&ignoredLines, &expectediglines, "vector<bool>", "PC202: ignored attribute successfully initialized");
 }
 
 void testgetNumTuples(){
@@ -29,11 +29,11 @@ void testgetNumTuples(){
     numLines = parser.getNumTuples();
     int expectedLines;
     expectedLines = 1;
-    test_equivalence(&numLines, &expectedLines, "int", "PC203");
+    test_equivalence(&numLines, &expectedLines, "int", "PC203: numLines getter returns expected value 1 after adding tuple");
     parser.RemoveCodeTuple(0);
     numLines = parser.getNumTuples();
     expectedLines = 0;
-    test_equivalence(&numLines, &expectedLines, "int", "PC204");
+    test_equivalence(&numLines, &expectedLines, "int", "PC204: numLines getter returns expected value 0 after removing 1 tuple");
 }
 
 void testgetlines(){
@@ -42,11 +42,11 @@ void testgetlines(){
     vector<string> reglines;
     reglines = parser.getLines();
     vector<string> expectedLines {"aaa"};
-    test_equivalence(&reglines, &expectedLines, "vector<string>", "PC205");
+    test_equivalence(&reglines, &expectedLines, "vector<string>", "PC205: lines getter returns vector with added string element after adding tuple");
     parser.RemoveCodeTuple(0);
     vector<string> reglines1 = parser.getLines();
     vector<string> expectedLines1;
-    test_equivalence(&reglines1, &expectedLines1, "vector<string>", "PC206");
+    test_equivalence(&reglines1, &expectedLines1, "vector<string>", "PC206: lines getter returns empty vector removing tuple");
 }
 
 void testgetbool(){
@@ -56,12 +56,12 @@ void testgetbool(){
     vector<bool> reglines;
     reglines = parser.getIgnores();
     vector<bool> expectedLines {false, true};
-    test_equivalence(&reglines, &expectedLines, "vector<bool>", "PC207");
+    test_equivalence(&reglines, &expectedLines, "vector<bool>", "PC207: ignored getter returns vector expected boolean after adding tuple");
     parser.RemoveCodeTuple(0);
     parser.RemoveCodeTuple(0);
     vector<bool> reglines1 = parser.getIgnores();
     vector<bool> expectedLines1;
-    test_equivalence(&reglines1, &expectedLines1, "vector<bool>", "PC208");
+    test_equivalence(&reglines1, &expectedLines1, "vector<bool>", "PC208: ignored getter returns empty vector after removing tuple");
 }
 
 void trickyAnnotationDefaultParserTest(){
@@ -70,10 +70,10 @@ void trickyAnnotationDefaultParserTest(){
     code = defaultParser.ParseCode("srcFiles/trickyAnnotation.cpp");
     vector<string> a = code->getLines();
     vector<string> expectedVec = {"// This is a test file for testing PPALMS Program\n#include <iostream>\nusing namespace std;\nint main(){\n","","","","    int x = 0;\n    int y = 0;\n","","","    int z = x + y;\n","    cout << z;\n","    return z;\n}\n"};
-    test_equivalence(&a, &expectedVec, "vector<string>", "DCP209");
+    test_equivalence(&a, &expectedVec, "vector<string>", "DCP209: Additional spaces around annotations do not affect parsing into lines attribute");
     vector<bool> b = code->getIgnores();
     vector<bool> expectedBool = {1,0,0,0,0,0,0,0,0,1};
-    test_equivalence(&b, &expectedBool, "vector<bool>", "DCP210");
+    test_equivalence(&b, &expectedBool, "vector<bool>", "DCP210: Additional spaces around annotations do not affect parsing into ignored attribute");
 
 }
 
@@ -88,7 +88,7 @@ void missingBlockAnnotationDefaultParserTest(){
     string actual = buffer.str();
 
     string expect = "Error: Unmatched Annotations";
-    test_equivalence(&actual, &expect, "string", "DCP211");
+    test_equivalence(&actual, &expect, "string", "DCP211: Having missing/incorrect annotations results in error message");
 
 }
 
@@ -98,10 +98,10 @@ void NoAnnotationDefaultParserTest(){
     code = defaultParser.ParseCode("srcFiles/noannotations.cpp");
     vector<string> a = code->getLines();
     vector<string> expectedVec = {"#include <iostream>\n","using namespace std;\n","","int main(){\n","    int x = 0;\n","    int y = 0;\n","    int z = x + y;\n","    cout << z;\n","    return z;\n","}\n"};
-    test_equivalence(&a, &expectedVec, "vector<string>", "DCP212");
+    test_equivalence(&a, &expectedVec, "vector<string>", "DCP212: Check that line attribute has correct elements with no annotations");
     vector<bool> b = code->getIgnores();
     vector<bool> expectedBool = {0,0,0,0,0,0,0,0,0,0};
-    test_equivalence(&b, &expectedBool, "vector<bool>", "DCP213");
+    test_equivalence(&b, &expectedBool, "vector<bool>", "DCP213: Check that ignored attribute has all false elements with no annotations");
 
 }
 
