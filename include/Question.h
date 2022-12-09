c4e70d0974e0c19b949ab45ab5e200c0058e3f25
lembap10 <baptiste.lemercier10@gmail.com>
2022-12-08 18:14:41 -0600

    question.h stuff

--- include/Question.h
+++ include/Question.h
@@ -2,16 +2,66 @@
 #define QUESTION_H
 
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 class Question {
     protected: 
-
+        int id;
+        string questionText;
+        string answer;
     public:
         /**
          * @brief Construct a new Question object
+         * 
          */
-        Question() {}
+        Question();
+
+        /**
+         * @brief Print the info regarding the question object
+         * 
+         */
+        virtual void Print() = 0;
+
+        /**
+         * @brief 
+         * 
+         */
+        string getAnswer();
+
+        /**
+         * @brief 
+         * 
+         */
+        void setAnswer(string answer);
+
+        /**
+         * @brief 
+         * 
+         * @return string 
+         */
+        string getQuestionText();
+
+        /**
+         * @brief Set the Question Text object
+         * 
+         */
+        void setQuestionText(string question);
+
+        /**
+         * @brief 
+         * 
+         * @return int 
+         */
+        int getID();
+
+        /**
+         * @brief 
+         * 
+         * @param ID 
+         */
+        void setID(int ID);
+
 };
 #endif
--- src/Question.cc
+++ src/Question.cc
@@ -1,2 +1,59 @@
 #include "../include/Question.h"
 using namespace std;
+
+/**
+ * @brief Construct a new Question object
+ * Initializes object values
+ */
+Question::Question(){
+    id = 0;
+    questionText = '';
+    answer= '';
+}
+
+
+string Question::getAnswer(){
+    return answer
+}
+
+/**
+ * @brief 
+ * 
+ */
+void Question::setAnswer(string answer){
+    this->answer = answer;
+}
+
+/**
+ * @brief 
+ * 
+ * @return string 
+ */
+string Question::getQuestionText(){
+    return questionText;
+}
+/**
+ * @brief Set the Question Text object
+ * 
+ */
+void Question::setQuestionText(string question){
+    this->questionText = question;
+}
+
+/**
+ * @brief returns the id of the question
+ * 
+ * @return int 
+ */
+int Question::getID(){
+    return id;
+}
+
+/**
+ * @brief function to get the question ID
+ * 
+ * @param ID 
+ */
+void Question::setID(int ID){
+    this->id = ID;
+}
