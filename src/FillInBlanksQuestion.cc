c578e6c796187111752fe309806454a327da2f9e
NathanL0518 <619478788cx@gmail.com>
2022-12-08 20:07:43 -0600

    added more functions

--- src/FillInBlanksQuestion.cc
+++ src/FillInBlanksQuestion.cc
@@ -10,6 +10,7 @@ int FillInBlanksQuestion::getLengthBlank() { return lengthBlank; }
 /**
  * @brief set the length of the blank
  *
+ * @param length: the length of the blank
  */
 void FillInBlanksQuestion::setLengthBlank(int length) {
   this->lengthBlank = length;
@@ -24,6 +25,7 @@ void FillInBlanksQuestion::getBlankStart() { return blankStart; }
 /**
  * @brief set the index of the start of the blank
  *
+ * @param index: the index of the start of the blank
  */
 void FillInBlanksQuestion::setBlankStart(int index) {
   this->blankStart = index;
